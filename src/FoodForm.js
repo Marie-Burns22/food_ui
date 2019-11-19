5bcf9a7e78dcea24e043fa563e594e9769993b4c
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-18 21:49:29 -0800

    Add two main files to render.

---
+++ src/FoodForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FoodForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This will be the food form page.</h1>
+            </div>
+        )
+    }
+}
---
+++ src/FoodList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FoodList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This will be the food list.</h1>
+            </div>
+        )
+    }
+}
