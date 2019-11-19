d453e08912cf9ba945a0e7d2fc6fbdcabc61a6c9
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-18 22:18:15 -0800

    Build basic form to add data to api.

--- src/FoodForm.js
+++ src/FoodForm.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
 
 export default class FoodForm extends Component {
     render() {
         return (
-            <div>
-                <h1>This will be the food form page.</h1>
-            </div>
+            <Form className="container">
+                <h1>Add Emission Data for Food</h1>
+                <Form.Group controlId="foodForm.nameInput">
+                    <Form.Label>Food Name</Form.Label>
+                    <Form.Control
+                        required 
+                        type="text" 
+                        placeholder="Food Name"
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="foodForm.category" >
+                    <Form.Label>Food Category</Form.Label>
+                    <Form.Control 
+                        required
+                        type="text"
+                        placeholder="Food Category"
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="foodForm.emissionsAmount" >
+                    <Form.Label>Emissions Data</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Emissions Data"
+                    />
+                </Form.Group>
+
+            </Form>
         )
     }
 }
