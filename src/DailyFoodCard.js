01fc5dbc0862e66ddf26a9f8de54cc13de3b35c2
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-22 11:26:40 -0800

    Update food cards to list food and servings.

--- src/DailyFoodCard.js
+++ src/DailyFoodCard.js
@@ -4,12 +4,12 @@ import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 export default function DailyFoodCard({dailyDiet, day}){
     
     return( 
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '35rem' }}>
             <Card.Header>{day}</Card.Header>
             <ListGroup className="list-group-flush">
-                <ListGroupItem>Cras justo odio</ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                {dailyDiet.map(entry => (
+                    <ListGroupItem key={entry.food.name}>Food: {entry.food.name}, Servings: {entry.servings}</ListGroupItem>
+                ))}
             </ListGroup>
         </Card>
     )
